f9dbdf70613318e6953c521f187ce79b709b5485
Takashi SASAKI <takashi316@gmail.com>
2013-10-06 19:50:59 +0900

    parsing resulting HTML returned by Everything Search Engine

--- Everything.js
+++ Everything.js
@@ -1,12 +1,19 @@
 /* for Everything serach engine */
 function Everything(port) {
-    if (port == null) port = 80;
-    var jqXHR = $.ajax({type: "GET", url: "http://localhost:" + port});
+    if (port == null) this.port = 80;
+    var jqXHR = $.ajax({type: "GET", url: "http://localhost:" + this.port, dataType: "html"});
     jqXHR.done(function (data, textStatus, jqXHR) {
+        var dom = $(data);
+        this.title = dom[3].innerText;
+        if (title == "Everything") {
+            this.isHttpAccepted = true;
+        } else {
+            this.isHttpAccepted = false;
+        }
         $("#textareaEverything").val(data);
     });
     jqXHR.fail(function (jqXHR, textStatus, errorThrown) {
         $("#textareaEverything").val(textStatus);
     });
 }
-var everything = new Everything(88);
+var everything = new Everything();

4e485e45ffdf7f21fc003d5c92f84a07558ba804
Takashi SASAKI <takashi316@gmail.com>
2013-12-17 22:32:33 +0900

    class Everything updated

--- Everything.js
+++ Everything.js
@@ -1,19 +1,42 @@
 /* for Everything serach engine */
+"use strict";
 function Everything(port) {
-    if (port == null) this.port = 80;
-    var jqXHR = $.ajax({type: "GET", url: "http://localhost:" + this.port, dataType: "html"});
-    jqXHR.done(function (data, textStatus, jqXHR) {
-        var dom = $(data);
-        this.title = dom[3].innerText;
-        if (title == "Everything") {
-            this.isHttpAccepted = true;
-        } else {
-            this.isHttpAccepted = false;
-        }
-        $("#textareaEverything").val(data);
-    });
-    jqXHR.fail(function (jqXHR, textStatus, errorThrown) {
-        $("#textareaEverything").val(textStatus);
-    });
-}
-var everything = new Everything();
+    if (port == null) port = 80;
+    var url = "http://localhost:" + port + "/";
+
+    this.run = function () {
+        console.log(url);
+        var jq_xhr = $.ajax(
+            {
+                url: url,
+                type: "GET",
+                dataType: "html"
+            }
+        );
+        jq_xhr.done(function (data, textStatus, jqXHR) {
+            console.log("ajax done");
+            var dom = $(data);
+            this.title = dom[3].innerText;
+            if (title == "Everything") {
+                this.isHttpAccepted = true;
+            } else {
+                this.isHttpAccepted = false;
+            }//if
+            $("#textareaEverything").val(data);
+        });
+
+        jq_xhr.fail(function (xmlHttpRequest, textStatus, errorThrown) {
+            console.log("ajax fail");
+            $("#textareaEverything").val(textStatus);
+        });
+
+        jq_xhr.always(function (jqXHR, textStatus, errorThrown) {
+            console.log("ajax always");
+        });
+    }
+//run
+}//Everything
+$(function () {
+    var everything = new Everything(80);
+    setInterval(everything.run, 5000);
+});
